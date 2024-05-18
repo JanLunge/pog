be8c525c195742f4592312e7ad970bc38638dfbd
Birkemosen <juliannielsen@gmail.com>
2024-05-18 09:29:10 +0200

    Move split side configuration to Pins tab

--- src/renderer/src/components/PinSetup.vue
+++ src/renderer/src/components/PinSetup.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="flex items-center justify-center">
-    <p class="max-w-md py-4">Define the mapping for columns and rows to the microcontroller pins</p>
-  </div>
-
-  <div v-if="keyboardStore.wiringMethod === 'matrix'" class="flex justify-center">
-    <div class="w-full max-w-md">
-      <p class="mb-2 text-sm">Diode Direction</p>
-      <select v-model="keyboardStore.diodeDirection" class="select-bordered select w-full">
-        <option value="COL2ROW">COL2ROW</option>
-        <option value="ROW2COL">ROW2COL</option>
-      </select>
-    </div>
+    <p class="max-w-md py-4">
+      Define the mapping for columns and rows to the microcontroller pins. For split keyboards
+      define the method for detecting the spilt side.
+    </p>
   </div>
   <div v-if="initialSetup" class="my-8 flex w-full justify-center">
     <button
-      class="btn-primary btn"
+      class="btn btn-primary"
       :class="{ 'btn-disabled': !pinsCompleted }"
       @click="$emit('next')"
     >
@@ -22,11 +15,16 @@
     </button>
   </div>
   <div class="mt-5 flex justify-center gap-8">
-    <div v-if="keyboardStore.wiringMethod === 'matrix'" class="flex-grow-0">
-      <div class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8" style="width: 350px">
+    <!-- LEFT COLUMN -->
+    <div class="flex-grow-0">
+      <div
+        v-if="keyboardStore.wiringMethod === 'matrix'"
+        class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8"
+        style="width: 350px"
+      >
         <p class="flex items-center justify-center pb-4 text-xl font-bold">
           Row Pins
-          <span class="badge-primary badge ml-2 font-bold">{{ keyboardStore.rowPins.length }}</span>
+          <span class="badge badge-primary ml-2 font-bold">{{ keyboardStore.rowPins.length }}</span>
         </p>
         <div
           v-for="(_pin, index) in keyboardStore.rowPins"
@@ -35,16 +33,20 @@
           <p class="mr-2 text-right">{{ index }}</p>
           <input
             v-model="keyboardStore.rowPins[index]"
-            class="input-bordered input input-sm col-span-4"
+            class="input input-bordered input-sm col-span-4"
             type="text"
             placeholder="17"
           />
         </div>
       </div>
-      <div class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8">
+      <div
+        v-if="keyboardStore.wiringMethod === 'matrix'"
+        class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8"
+        style="width: 350px"
+      >
         <p class="flex items-center justify-center pb-4 text-xl font-bold">
           Column Pins
-          <span class="badge-primary badge ml-2 font-bold">{{ keyboardStore.colPins.length }}</span>
+          <span class="badge badge-primary ml-2 font-bold">{{ keyboardStore.colPins.length }}</span>
         </p>
         <div
           v-for="(_pin, index) in keyboardStore.colPins"
@@ -53,18 +55,20 @@
           <span class="mr-2 text-right">{{ index }}</span>
           <input
             v-model="keyboardStore.colPins[index]"
-            class="input-bordered input input-sm col-span-4"
+            class="input input-bordered input-sm col-span-4"
             type="text"
             placeholder="17"
           />
         </div>
       </div>
-    </div>
-    <div v-if="keyboardStore.wiringMethod === 'direct'" class="flex-grow-0">
-      <div class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8" style="width: 350px">
+      <div
+        v-if="keyboardStore.wiringMethod === 'direct'"
+        class="mb-4 grid grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8"
+        style="width: 350px"
+      >
         <p class="flex items-center justify-center pb-4 text-xl font-bold">
           Direct Pins
-          <span class="badge-primary badge ml-2 font-bold">{{
+          <span class="badge badge-primary ml-2 font-bold">{{
             keyboardStore.directPins.length
           }}</span>
         </p>
@@ -75,28 +79,104 @@
           <p class="mr-2 text-right">{{ index }}</p>
           <input
             v-model="keyboardStore.directPins[index]"
-            class="input-bordered input input-sm col-span-4"
+            class="input input-bordered input-sm col-span-4"
             type="text"
             placeholder="17"
           />
         </div>
       </div>
+      <div
+        v-if="showVbusOption"
+        class="mb-4 grid w-full grid-cols-1 gap-2 rounded bg-base-100 p-2 py-8"
+        style="width: 350px"
+      >
+        <p class="flex items-center justify-center pb-4 text-xl font-bold">
+          Split Pins <span class="badge badge-primary ml-2 font-bold">{{ numberOfSplitPins }}</span>
+        </p>
+        <div v-if="keyboardStore.splitSide === 'vbus'">
+          <label class="mb-2 flex items-center gap-2">
+            <span>VBUS Pin</span>
+            <input
+              v-model="keyboardStore.vbusPin"
+              type="text"
+              class="input input-bordered input-sm col-span-4"
+            />
+          </label>
+          <p class="px-2 py-2 text-sm italic">{{ vbusPinHint }}</p>
+        </div>
+        <div>
+          <label
+            v-if="keyboardStore.keyboardType === 'splitOnewire'"
+            class="mb-2 flex items-center gap-2"
+          >
+            <span class="mr-2 text-right">SplitPin</span>
+            <input
+              v-model="keyboardStore.splitPinA"
+              type="text"
+              class="input input-bordered input-sm col-span-4"
+            />
+          </label>
+          <label
+            v-if="keyboardStore.keyboardType === 'splitSerial'"
+            class="mb-2 flex items-center gap-2"
+          >
+            <span class="mr-2 text-right">SplitPin A</span>
+            <input
+              v-model="keyboardStore.splitPinA"
+              type="text"
+              class="input input-bordered input-sm col-span-4"
+            />
+          </label>
+          <label
+            v-if="keyboardStore.keyboardType === 'splitSerial'"
+            class="mb-2 flex items-center gap-2"
+          >
+            <span class="mr-2 text-right">SplitPin B</span>
+            <input
+              v-model="keyboardStore.splitPinB"
+              type="text"
+              class="input input-bordered input-sm col-span-4"
+            />
+          </label>
+          <p class="px-2 py-2 text-sm italic">{{ splitPinHint }}</p>
+        </div>
+      </div>
     </div>
-
+    <!-- RIGHT COLUMN -->
     <div class="flex w-1/2 flex-col items-center" style="width: 400px">
-      <div class="w-full">
+      <div class="mb-4 w-full">
         <p class="mb-2 text-sm">Pin Prefix</p>
-        <select v-model="keyboardStore.pinPrefix" class="select-bordered select mb-2 w-full">
+        <select v-model="keyboardStore.pinPrefix" class="select select-bordered mb-2 w-full">
           <option value="gp">GP</option>
           <option value="none">none</option>
           <option value="board">board</option>
           <option value="quickpin">quickpin</option>
         </select>
-        <p>{{ pinPfrefixHint }}</p>
+        <p class="px-2 py-2 text-sm italic">{{ pinPrefixHint }}</p>
       </div>
+
+      <div v-if="keyboardStore.wiringMethod === 'matrix'" class="mb-4 w-full">
+        <p class="mb-2 text-sm">Diode Direction</p>
+        <select v-model="keyboardStore.diodeDirection" class="select select-bordered mb-2 w-full">
+          <option value="COL2ROW">COL2ROW</option>
+          <option value="ROW2COL">ROW2COL</option>
+        </select>
+      </div>
+
+      <div v-if="keyboardStore.isSplit()" class="mb-4 w-full">
+        <p class="mb-2 text-sm">Split Side Detection</p>
+        <select v-model="keyboardStore.splitSide" class="select select-bordered mb-2 w-full">
+          <option value="left">Config (Left)</option>
+          <option value="right">Config (Right)</option>
+          <option v-if="showVbusOption" value="vbus">VBUS</option>
+          <option value="label">Drive Label (L/R)</option>
+        </select>
+        <p class="px-2 py-2 text-sm italic">{{ splitSideHint }}</p>
+      </div>
+
       <div class="mt-4 w-full">
         <p class="mb-2 text-sm">Microcontroller</p>
-        <select v-model="keyboardStore.controller" class="select-bordered select w-full">
+        <select v-model="keyboardStore.controller" class="select select-bordered w-full">
           <option value="0xcb-helios">0xCB Helios</option>
           <option value="">other</option>
         </select>
@@ -145,7 +225,7 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 // import { useRouter } from 'vue-router'
-import { keyboardStore, pinPfrefixHint } from '../store'
+import { keyboardStore, pinPrefixHint, splitSideHint, vbusPinHint, splitPinHint } from '../store'
 // const router = useRouter()
 
 defineProps(['initialSetup'])
@@ -180,8 +260,21 @@ const pinsCompleted = computed(() => {
   if (keyboardStore.rowPins.includes('')) return false
   return true
 })
-</script>
 
+const showVbusOption = computed(() => {
+  return (
+    keyboardStore.keyboardType === 'splitSerial' || keyboardStore.keyboardType === 'splitOnewire'
+  )
+})
+
+const numberOfSplitPins = computed(() => {
+  let pins = 0
+  if (keyboardStore.splitSide === 'vbus') pins++
+  if (keyboardStore.keyboardType === 'splitSerial') return pins + 2
+  if (keyboardStore.keyboardType === 'splitOnewire') return pins + 1
+  return pins
+})
+</script>
 <style lang="scss" scoped>
 .controller-labels {
   @apply absolute grid;
