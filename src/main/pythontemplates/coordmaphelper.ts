1c7af1c281bd328f000542e4372fb5689705d260
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-06-09 21:33:59 +0200

    Updated coordmaphelper, only prints out "test" atm

--- src/main/pythontemplates/coordmaphelper.ts
+++ src/main/pythontemplates/coordmaphelper.ts
@@ -1,5 +1,5 @@
 export const coordmaphelperpy = `# coordmaphelper.py helper script to get your coord map - v0.9.5
-from kmk.handlers.sequences import simple_key_sequence
+from kmk.modules.macros import Macros
 from kmk.keys import KC
 from kb import KMKKeyboard as _KMKKeyboard
 import pog
@@ -10,22 +10,18 @@ class KMKKeyboard(_KMKKeyboard):
         print("press each key to get its coord_mapping value")
     # *2 for split keyboards, which will typically manage twice the number of keys
     # of one side. Having this N too large will have no impact (maybe slower boot..)
-    N = pog.keyCount * 2
-    coord_mapping = list(range(N))
+        N = pog.keyCount * 2
+        self.coord_mapping = list(range(N))
+        self.modules.append(Macros())
 
-    layer = []
+        layer = []
 
-    for i in range(N):
-        c, r = divmod(i, 100)
-        d, u = divmod(r, 10)
-        layer.append(
-            simple_key_sequence(
-                (
-                    getattr(KC, "N" + str(c)),
-                    getattr(KC, "N" + str(d)),
-                    getattr(KC, "N" + str(u)),
-                    KC.SPC,
-                )
-            )
-        )
-    keymap = [layer]`
+        for i in range(N):
+            c, r = divmod(i, 100)
+            d, u = divmod(r, 10)
+            layer.append(
+     			KC.MACRO(
+    	        	"test"
+        		)
+   			)
+        self.keymap = [layer]`
