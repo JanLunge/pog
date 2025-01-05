14ff23dc8beda80e7544599a1bd47978cdd7e3e3
Jan Lunge <hello@janlunge.de>
2025-01-05 12:27:19 +0100

    Enhance KeyLayoutInfo component with improved key selection UI and logic. Added buttons for navigating between keys when one key is selected, and updated the display to show a message when no keys are selected. Refactored key update logic to create a new layout copy and only apply changes to modified properties. Improved handling of selected keys and layout updates.

--- src/renderer/src/components/KeyLayoutInfo.vue
+++ src/renderer/src/components/KeyLayoutInfo.vue
@@ -1,6 +1,37 @@
 <template>
-  <p class="font-bold">Key info for key #{{ [...selectedKeys][0] }}</p>
-  <p>{{ selectedKeys.size }} {{ selectedKeys.size == 1 ? 'key' : 'keys' }} selected:</p>
+  
+    <div class="flex justify-between items-center h-10">
+      <div v-if="selectedKeys.size === 0">
+        <p class="font-bold">No keys selected</p>
+      </div>
+      <div v-else>
+        <p class="font-bold">
+          <template v-if="selectedKeys.size === 1">
+            Key #{{ [...selectedKeys][0] }}
+          </template>
+          <template v-else>
+            {{ selectedKeys.size }} keys selected
+          </template>
+        </p>
+      </div>
+      <div v-if="selectedKeys.size === 1" class="flex gap-2">
+        <button 
+          class="btn btn-sm" 
+          :disabled="[...selectedKeys][0] === 0"
+          @click="selectPreviousKey"
+        >
+          Previous Key
+        </button>
+        <button 
+          class="btn btn-sm" 
+          :disabled="[...selectedKeys][0] === layout.length - 1"
+          @click="selectNextKey"
+        >
+          Next Key
+        </button>
+      </div>
+  </div>
+  <hr class="border-base-300">
   <p class="mt-2 text-sm">Basics</p>
   <div class="flex flex-col gap-2">
     <div class="grid grid-cols-4 gap-2 text-right">
@@ -160,11 +191,13 @@
 </template>
 
 <script lang="ts" setup>
-import { selectedKeys } from '../store'
+import { Key, selectedKeys } from '../store'
 import { computed, ref, watch } from 'vue'
 import { isNumber } from '@vueuse/core'
 import { keyboardStore } from '../store'
-const props = defineProps(['layout'])
+const props = defineProps<{
+  layout: Key[]
+}>()
 
 const tmpKey = ref<{
   x: number | ''
@@ -220,7 +253,9 @@ const getSameKeyAttrs = (keys) => {
 }
 const updateSelectedKey = () => {
   console.log('updating selected keys')
-  if ([...selectedKeys.value].length === 1) {
+  console.log(props.layout)
+  console.log(selectedKeys.value)
+  if ([...selectedKeys.value].length === 1 && props.layout?.length > 0) {
     const keyToLoad = props.layout[[...selectedKeys.value][0]]
 
     // only load overlapping data from all selected keys
@@ -260,71 +295,129 @@ const updateSelectedKey = () => {
 }
 updateSelectedKey()
 
-watch(selectedKeys.value, () => {
-  updateSelectedKey()
-})
+watch(
+  () => new Set(selectedKeys.value),
+  (newVal) => {
+    console.log('selected keys changed', newVal)
+    updateSelectedKey()
+  },
+  { deep: true }
+)
+
+const emit = defineEmits(['update:layout'])
 
 const updateKey = () => {
+  // Create a new copy of the layout to modify
+  const newLayout = [...props.layout]
+  
   selectedKeys.value.forEach((keyIndex) => {
-    console.log('updating key with index', keyIndex, props.layout[keyIndex])
-    // only modify if a field has a value
-    // validate all fields and remove things that are set to default
-    if (tmpKey.value.x !== '') props.layout[keyIndex].x = Number(tmpKey.value.x)
-    if (tmpKey.value.x2 !== '') props.layout[keyIndex].x2 = Number(tmpKey.value.x2)
-    if (tmpKey.value.y2 !== '') props.layout[keyIndex].y2 = Number(tmpKey.value.y2)
-    if (tmpKey.value.y !== '') props.layout[keyIndex].y = Number(tmpKey.value.y)
-    if (tmpKey.value.w !== '') props.layout[keyIndex].w = Number(tmpKey.value.w)
-    if (tmpKey.value.h !== '') props.layout[keyIndex].h = Number(tmpKey.value.h)
-    if (tmpKey.value.w2 !== '') props.layout[keyIndex].w2 = Number(tmpKey.value.w2)
-    if (tmpKey.value.h2 !== '') props.layout[keyIndex].h2 = Number(tmpKey.value.h2)
-    if (tmpKey.value.r !== '') props.layout[keyIndex].r = Number(tmpKey.value.r)
-    if (tmpKey.value.rx !== '') props.layout[keyIndex].rx = Number(tmpKey.value.rx)
-    if (tmpKey.value.ry !== '') props.layout[keyIndex].ry = Number(tmpKey.value.ry)
-    if (tmpKey.value.coordMapIndex !== '' && !isNaN(tmpKey.value.coordMapIndex as any))
-      props.layout[keyIndex].coordMapIndex = Number(tmpKey.value.coordMapIndex)
-    else props.layout[keyIndex].coordMapIndex = ''
-    if (tmpKey.value.encoderIndex !== '' && !isNaN(Number(tmpKey.value.encoderIndex)))
-      props.layout[keyIndex].encoderIndex = Number(tmpKey.value.encoderIndex)
-    else props.layout[keyIndex].encoderIndex = undefined
-    // if (
-    //   (tmpKey.value.matrix &&
-    //     tmpKey.value.matrix.length == 2 &&
-    //     tmpKey.value.matrix[0] !== "") ||
-    //   tmpKey.value.matrix[1] !== ""
-    // )
-    //   props.layout[keyIndex].matrix = tmpKey.value.matrix.map(
-    //     (a: string | number) => {
-    //       if (a === "NaN" || a === "") return NaN;
-    //       return Number(a);
-    //     }
-    //   );
-    if (tmpKey.value.matrix && tmpKey.value.matrix[0] !== '') {
-      console.log('updating matrix 0', tmpKey.value.matrix, props.layout[keyIndex].marix)
-      if (!Array.isArray(props.layout[keyIndex].matrix)) {
-        props.layout[keyIndex].matrix = [Number(tmpKey.value.matrix[0]), NaN]
-      } else {
-        props.layout[keyIndex].matrix[0] = Number(tmpKey.value.matrix[0])
+    // Create a new object with only the modified properties
+    const updates: Partial<(typeof props.layout)[0]> = {}
+
+    // Only add properties to updates if they have a non-empty value
+    if (tmpKey.value.x !== '') updates.x = Number(tmpKey.value.x)
+    if (tmpKey.value.x2 !== '') updates.x2 = Number(tmpKey.value.x2)
+    if (tmpKey.value.y2 !== '') updates.y2 = Number(tmpKey.value.y2)
+    if (tmpKey.value.y !== '') updates.y = Number(tmpKey.value.y)
+    if (tmpKey.value.w !== '') updates.w = Number(tmpKey.value.w)
+    if (tmpKey.value.h !== '') updates.h = Number(tmpKey.value.h)
+    if (tmpKey.value.w2 !== '') updates.w2 = Number(tmpKey.value.w2)
+    if (tmpKey.value.h2 !== '') updates.h2 = Number(tmpKey.value.h2)
+    if (tmpKey.value.r !== '') updates.r = Number(tmpKey.value.r)
+    if (tmpKey.value.rx !== '') updates.rx = Number(tmpKey.value.rx)
+    if (tmpKey.value.ry !== '') updates.ry = Number(tmpKey.value.ry)
+
+    // Handle coordMapIndex only if it was explicitly changed
+    if (tmpKey.value.coordMapIndex !== '') {
+      updates.coordMapIndex = !isNaN(tmpKey.value.coordMapIndex as any)
+        ? Number(tmpKey.value.coordMapIndex)
+        : ''
+    }
+
+    // Handle encoderIndex only if it was explicitly changed
+    if (tmpKey.value.encoderIndex !== '') {
+      updates.encoderIndex = !isNaN(Number(tmpKey.value.encoderIndex))
+        ? Number(tmpKey.value.encoderIndex)
+        : undefined
+    }
+
+    // Handle matrix updates
+    if (tmpKey.value.matrix) {
+      if (tmpKey.value.matrix[0] !== '') {
+        if (!Array.isArray(newLayout[keyIndex].matrix)) {
+          updates.matrix = [Number(tmpKey.value.matrix[0]), NaN]
+        } else {
+          updates.matrix = [...newLayout[keyIndex].matrix]
+          updates.matrix[0] = Number(tmpKey.value.matrix[0])
+        }
       }
-      console.log(props.layout[keyIndex].matrix)
+      if (tmpKey.value.matrix[1] !== '') {
+        updates.matrix = Array.isArray(updates.matrix)
+          ? updates.matrix
+          : Array.isArray(newLayout[keyIndex].matrix)
+          ? [...newLayout[keyIndex].matrix]
+          : [NaN, NaN]
+        updates.matrix[1] = Number(tmpKey.value.matrix[1])
+      }
     }
-    if (tmpKey.value.matrix && tmpKey.value.matrix[1] !== '') {
-      if (!props.layout[keyIndex].matrix) props.layout[keyIndex].matrix = [NaN, NaN]
-      props.layout[keyIndex].matrix[1] = Number(tmpKey.value.matrix[1])
+
+    // Handle variant updates
+    if (tmpKey.value.variant) {
+      if (tmpKey.value.variant[0] !== '') {
+        updates.variant = Array.isArray(newLayout[keyIndex].variant)
+          ? [...newLayout[keyIndex].variant]
+          : [NaN, NaN]
+        updates.variant[0] = Number(tmpKey.value.variant[0])
+      }
+      if (tmpKey.value.variant[1] !== '') {
+        updates.variant = Array.isArray(updates.variant)
+          ? updates.variant
+          : Array.isArray(newLayout[keyIndex].variant)
+          ? [...newLayout[keyIndex].variant]
+          : [NaN, NaN]
+        updates.variant[1] = Number(tmpKey.value.variant[1])
+      }
     }
-    if (tmpKey.value.variant && tmpKey.value.variant[0] !== '') {
-      if (!props.layout[keyIndex].variant) props.layout[keyIndex].variant = [NaN, NaN]
-      props.layout[keyIndex].variant[0] = Number(tmpKey.value.variant[0])
-    }
-    if (tmpKey.value.variant && tmpKey.value.variant[1] !== '') {
-      if (!props.layout[keyIndex].variant) props.layout[keyIndex].variant = [NaN, NaN]
-      props.layout[keyIndex].variant[1] = Number(tmpKey.value.variant[1])
-    }
+
+    // Update the key in our new layout copy
+    newLayout[keyIndex] = { ...newLayout[keyIndex], ...updates }
+    savePartialLayout(newLayout)
   })
+
+  // Emit the entire updated layout once
+  emit('update:layout', newLayout)
 }
 
 const matrixValid = computed(() => {
   return !isNumber(tmpKey.value.matrix[0]) || !isNumber(tmpKey.value.matrix[1])
 })
+
+const savePartialLayout = (newLayout: Key[]) => {
+  console.log('saving partial layout', newLayout)
+  newLayout.forEach((key, i) => {
+    // Only update specific properties that can be changed in the layout editor
+    const keyProps = ['x', 'y', 'x2', 'y2', 'w', 'h', 'w2', 'h2', 'r', 'rx', 'ry', 'matrix', 'variant', 'coordMapIndex', 'encoderIndex']
+    keyProps.forEach(prop => {
+      if (key[prop] !== undefined && key[prop] !== keyboardStore.keys[i][prop]) {
+        keyboardStore.keys[i][prop] = key[prop]
+      }
+    })
+  })
+}
+
+const selectNextKey = () => {
+  const currentKey = [...selectedKeys.value][0]
+  if (currentKey < props.layout.length - 1) {
+    selectedKeys.value = new Set([currentKey + 1])
+  }
+}
+
+const selectPreviousKey = () => {
+  const currentKey = [...selectedKeys.value][0]
+  if (currentKey > 0) {
+    selectedKeys.value = new Set([currentKey - 1])
+  }
+}
 </script>
 
 <style lang="scss" scoped>
