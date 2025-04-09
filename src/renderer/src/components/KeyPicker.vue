061a1011702c5eed0d70c13d83dc438411b420c3
StoyanDimitrov <StoyanDimitrov@users.noreply.github.com>
2025-04-10 00:42:05 +0300

    Update KeyPicker.vue

--- src/renderer/src/components/KeyPicker.vue
+++ src/renderer/src/components/KeyPicker.vue
@@ -124,7 +124,6 @@
       <div class="bonus">
         <div class="key" @click="setKey('KC.RESET')">Reset</div>
         <div class="key" @click="setKey('KC.RELOAD')">Reload</div>
-        <div class="key" @click="setKey('KC.DEBUG')">Debug</div>
         <div class="key" @click="setKey('KC.BKDL')">BKDL</div>
       </div>
     </div>
@@ -132,8 +131,8 @@
       <div class="bonus">
         <div class="key" @click="setKey('KC.MPLY')">Play Pause</div>
         <div class="key" @click="setKey('KC.MUTE')">Mute</div>
-        <div class="key" @click="setKey('KC.VOLD')">Vol Down</div>
-        <div class="key" @click="setKey('KC.VOLU')">Vol Up</div>
+        <div class="key" @click="setKey('KC.VOLU')">Vol ↑</div>
+        <div class="key" @click="setKey('KC.VOLD')">Vol ↓</div>
         <div class="key" @click="setKey('KC.MFFD')">next track (OSX)</div>
         <div class="key" @click="setKey('KC.MRWD')">prev track (OSX)</div>
 
@@ -150,8 +149,12 @@
     <div v-if="category === 'rgb'" class="key-chooser flex">
       <div class="bonus">
         <div class="key" @click="setKey('KC.RGB_TOG')">RGB Toggle</div>
-        <div class="key" @click="setKey('KC.RGB_HUI')">RGB Hue increase</div>
-        <div class="key" @click="setKey('KC.RGB_HUD')">RGB Hue decrease</div>
+        <div class="key" @click="setKey('KC.RGB_HUI')">RGB Hue ↑</div>
+        <div class="key" @click="setKey('KC.RGB_HUD')">RGB Hue ↓</div>
+        <div class="key" @click="setKey('KC.RGB_SAI')">RGB Sat ↑</div>
+        <div class="key" @click="setKey('KC.RGB_SAD')">RGB Sat ↓</div>
+        <div class="key" @click="setKey('KC.RGB_VAI')">RGB Val ↑</div>
+        <div class="key" @click="setKey('KC.RGB_VAD')">RGB Val ↓</div>
       </div>
     </div>
     <div v-if="category === 'advanced'">
